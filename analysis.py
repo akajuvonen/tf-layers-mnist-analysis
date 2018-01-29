d73d51b1ab6841064067a26256bb02837ac653f8
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-01-29 21:20:47 +0200

    Added more layers to the network

--- analysis.py
+++ analysis.py
@@ -11,12 +11,21 @@ def cnn_model(input_layer):
                              kernel_size=[3, 3], padding="same",
                              activation=tf.nn.relu)
 
-    conv2 = tf.layers.conv2d(inputs=pool1, filters=16, kernel_size=[3, 3],
+    conv2 = tf.layers.conv2d(inputs=conv1, filters=16, kernel_size=[3, 3],
                              padding="same", activation=tf.nn.relu)
 
     # 2D pooling layer
-    pool = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    pool1 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
+    # The second set of convolutional and pooling layers
+    conv3 = tf.layers.conv2d(inputs=pool1, filters=32, kernel_size=[3, 3],
+                             padding="same", activation=tf.nn.relu)
+
+    conv4 = tf.layers.conv2d(inputs=conv3, filters=32, kernel_size=[3, 3],
+                             padding="same", activation=tf.nn.relu)
+
+    # 2D pooling layer
+    pool = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
 
 def main(dummy):
     pass
