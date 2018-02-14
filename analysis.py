f76611780a4006c8bd91adba3899327c9d278fed
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-02-14 13:06:33 +0200

    Changed pool to pool2, removed unneeded line breaks

--- analysis.py
+++ analysis.py
@@ -13,22 +13,18 @@ def cnn_model(features, labels, mode):
     conv1 = tf.layers.conv2d(inputs=input_layer, filters=16,
                              kernel_size=[3, 3], padding="same",
                              activation=tf.nn.relu)
-
     conv2 = tf.layers.conv2d(inputs=conv1, filters=16, kernel_size=[3, 3],
                              padding="same", activation=tf.nn.relu)
-
     # 2D pooling layer
     pool1 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
     # The second set of convolutional and pooling layers
     conv3 = tf.layers.conv2d(inputs=pool1, filters=32, kernel_size=[3, 3],
                              padding="same", activation=tf.nn.relu)
-
     conv4 = tf.layers.conv2d(inputs=conv3, filters=32, kernel_size=[3, 3],
                              padding="same", activation=tf.nn.relu)
-
     # 2D pooling layer
-    pool = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
+    pool2 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
 
 def main(dummy):
     # Load MNIST data
